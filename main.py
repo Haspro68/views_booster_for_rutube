ef82d2b2b46f6ea2328afd39c4b55015b55d3104
haspro <has.pro@yandex.ru>
2024-06-18 18:08:30 +0300

    Add

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-import sys
 import time
 import concurrent.futures
 import random
+from settings import deep_view, url
 
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.webdriver import WebDriver as Chrome
-#from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 
-user_agents = [
-    'hello',
-    'Mozilla/5.0 (Linux; Android 9; EVR-AL00 Build/HUAWEIEVR-AL00) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.64 HuaweiBrowser/10.0.1.332 Mobile Safari/537.36',
-    'Mozilla/5.0 (Linux; U; Android 8.0.0; en-gb; SM-G950F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.98 Mobile Safari/537.36 PHX/14.7',
-]
-#url, duration = str(sys.argv[1]), int(sys.argv[2])
-#url = 'https://2ip.ru/'
-url = 'https://whatmyuseragent.com/'
-duration = 20
+
+url = url
+duration = deep_view
 proxies = []
+user_agents = []
+logins = []
+passwords = []
+accounts = []
+acc_dict = {}
+
+
+try:
+    with open('accounts.txt', 'r') as ac:
+        accounts = ac.readlines()
+    for elem in accounts:
+        logins.append(elem.split(';')[0])
+        passwords.append(elem.strip().split(';')[1])
+    acc_dict = {logins[i]: passwords[i] for i in range(len(logins))}
+except:
+    pass
+
+
 try:
     with open('proxy_file.txt', 'r') as file:
         proxies=file.readlines()
@@ -26,16 +38,26 @@ except:
 print('Total proxies: ', len(proxies))
 
 
+try:
+    with open('user_agents.txt', 'r') as users:
+        user_agents=users.readlines()
+except:
+    print('Error')
+print('Total user agents: ', len(user_agents))
+
+
 def view_boost(proxy):
     options = ChromeOptions()
     options.add_argument('--proxy-server=%s' %proxy)
     options.add_argument(f'user-agent={random.choice(user_agents)}')
-    #options.add_argument('window-size=640, 480')
+    options.add_argument('window-size=640, 480')
 
     try:
         service = ChromeService(executable_path='/usr/bin/chromedriver')
         driver = Chrome(options=options, service=service)
         driver.get(url)
+        ####
+        ####
         time.sleep(duration)
     except:
         pass
@@ -43,5 +65,56 @@ def view_boost(proxy):
     driver.quit()
 
 
+def view_boost_with_account(proxy):
+    options = ChromeOptions()
+    options.add_argument('--proxy-server=%s' %proxy)
+    options.add_argument(f'user-agent={random.choice(user_agents)}')
+    options.add_argument('window-size=640, 480')
+
+    try:
+        service = ChromeService(executable_path='/usr/bin/chromedriver')
+        driver = Chrome(options=options, service=service)
+        driver.get(url)
+        login = random.choice(list(acc_dict))
+        time.sleep(10)
+        button = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div/div[1]/div/header/section[2]/div[4]/button')
+        button.click()
+        time.sleep(20)
+        login_input = driver.find_element(by=By.ID, value='phone-or-email-login')
+        login_input.clear()
+        login_input.send_keys(login)
+        submit_login = driver.find_element(by=By.ID, value='submit-login-continue')
+        submit_login.click()
+        time.sleep(5)
+        password_input = driver.find_element(by=By.ID, value='login-password')
+        password_input.clear()
+        password_input.send_keys(acc_dict[login])
+        submit_login_final = driver.find_element(by=By.ID, value='submit-login')
+        submit_login_final.click()
+        time.sleep(5)
+        activity_or_not = random.randint(0, 1)
+        if activity_or_not == 0:
+            time.sleep(duration)
+            driver.quit()
+        else:
+            time.sleep(duration)
+            like = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div/div[3]/div/main/div[1]/div[1]/section/div/div[1]/section[2]/div/div/section[1]/div[2]/section')
+            like.click()
+            time.sleep(1)
+            subscribe = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div/div[3]/div/main/div[1]/div[1]/section/div/div[1]/section[2]/div/div/section[2]/div/div/div[2]/div/button')
+            subscribe.click()
+            time.sleep(2)
+            driver.quit()
+    except:
+        pass
+
+
+
+#for _ in range(count):
+#   log_or_no = random.randint(0, 1)
+#    if log_or_no == 0:
+#        with concurrent.futures.ThreadPoolExecutor() as executor:
+#            executor.map(view_boost, proxies)
+#    else:
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(view_boost, proxies)
+    executor.map(view_boost_with_account, proxies)
---
+++ settings.py
@@ -0,0 +1,13 @@
+import random
+
+
+# Глубина просмотра в секундах от и до:
+min_deep_view = 1000
+max_deep_view = 1500
+deep_view = random.randint(min_deep_view, max_deep_view)
+
+
+#Ссылка на видео
+url = 'https://rutube.ru/video/2caa27c4926768a14395b856a7cfa580/'
+
+
